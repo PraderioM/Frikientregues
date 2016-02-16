35b0fd6749d4ac0f249ad642d7d2db4d79bb80c8
PraderioM <praderiom@gmail.com>
2016-02-16 13:45:45 +0100

    modificació del prograa per tal de introduir els operadors bit a bit >> i <<

--- frikientrega1.c
+++ frikientrega1.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(){
 	int i, LB;
 	double z;
-	z=1;
+	z=(double)1;
 	//aquesta part comentada del codi ensenya, en ordre, els 4 bytes que ocupa el nombre z. en la memoria de l'ordinador. 
 	/*for (i=0; i<8; i++){
 		printf("%d\n", (int)(((unsigned char*)&z)[i]));	
@@ -20,17 +19,26 @@ int main(){
 		LB=-1;
 	}
 	printf("\nA més a més agafant un nombre enter com ara %d i guardant-lo en un double obtenim un resultat ", (int)(z));
-	i=3.5+LB*2.5;
-	//Una representació subnormal ha de complir les condicions especificades per aquest if o sigui que el bit del signe
-	//diferent de 0 i l'exponent sigui 0.
-	if ((int)(((unsigned char*)&z)[i])<16 && (int)(((unsigned char*)&z)[i+LB])==128){
-		printf("sub");
+	i=3.5+LB*3.5;
+	//Una representació subnormal ha de complir les condicions especificades per aquest if o sigui que l'exponent sigui.
+	//0 (el signe pot ser 0 o 1 o sigui que el primer byte pot ser 0 o 128) i que la mantissa sigui diferent de 0.
+	if ((int)(((unsigned char*)&z)[i])==0 || (int)(((unsigned char*)&z)[i])==128){
+		if ((((unsigned char*)&z)[i-LB]>>4)==0){
+			int j, k;
+			k=0;
+			for (j=1; j<8; j++){
+				k+=(int)(((unsigned char*)&z)[i-j*LB]);
+			}
+			if (k!=0){
+				printf("sub");
+			}
+		}
 	}
 	printf("normal.\n");
 
 	double x,y;
 	int n, aux;
-	printf("\nintrodueix un nombre\n");
+	printf("\nintrodueix un nombre.\n");
 	scanf("%lf",&x);
 	//imposem que el signe sigui positiu per tal de que es guardi com a 0 en els 8 bytes reservats al double x.
 	if (x<0){
@@ -48,17 +56,16 @@ int main(){
 		printf("%d\t%d\n",(int)(((unsigned char*)&y)[i]), i);
 	}*/
 
-	//dividint entre 16 i agafant la part entera (com que n és un en integer guardarà únicament la part entera) eliminem
-	//els últims cuatre bits del byte que estem examinant i ens quedem únicament amb els bits que guarden la informació
-	//del exponent.
-	n=floor(n/16);
+	//utilitzant l'operador >> eliminem els últims cuatre bits del byte que estem examinant i ens quedem únicament amb
+	//els bits que guarden la informació del exponent.
+	n=n>>4;
 	//Tenint en compte la endianitat de l'ordinador agafem el byte on es guarda la primera part del exponent l'exponent.
 	//Aquest serà el que ocupi la posiciò 7 en el Little-endian i el que ocupi la posició 0 en el Big-endian.
 	i=3.5+LB*3.5;
 	aux=((int)(((unsigned char*)&y)[i]));
-	//multiplicant per 2^4 el nombre guardat en el byte que guarda la primera part del exponent obtenim floor(e)*16 on
-	//e és l'exponent y sumant el nombre trobat anteriorment trobem l'exponent normalitzat.
-	n+=aux*16;
+	//aplicant l'operador <<4 sobre el nombre guardat en el byte que guarda la primera part del exponent obtenim
+	//floor(e)*16 on e és l'exponent y sumant el nombre trobat anteriorment trobem l'exponent normalitzat.
+	n+=aux<<4;
 	printf("\nL'exponent normalitzat de %g és %d.\n", x, n);
 	n-=1023;
 	printf("\nLa part entera del logaritme en base 2 de %g és %d.\n", y, n);
@@ -77,7 +84,7 @@ int main(){
 	//sumant 128 i restant 256 en cas de que el resultat sigui mes gran o igual a 256 (cosa que es fa automàticament).
 	i=3.5+LB*3.5;
 	((unsigned char*)&y)[i]+=128;
-	printf("\n%g multiplicat per -2 dona %g\n", x, y);
+	printf("\n%g multiplicat per -2 dona %g.\n", x, y);
 	//Dividim ara y entre 4 per obtindre x dividit entre -2. Per tal de fer-ho només hem de restar 2 al exponent en
 	//base 2 de y i això s'obté restant 32 al byte que guarda la segona part del exponent tenint en compte que 
 	//l'exponent es guarda en dos bytes diferents.
@@ -89,6 +96,6 @@ int main(){
 	else{
 		((unsigned char*)&y)[i]-=32;
 	}
-	printf("\nMentres que %g dividit per -2 dona %g\n\n", x, y);
+	printf("\nMentres que %g dividit per -2 dona %g.\n\n", x, y);
 	return 0;
 }
